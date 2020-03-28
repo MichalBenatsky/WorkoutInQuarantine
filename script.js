e1b3d6bdaabcd91de7353f1c7bad2fd09b3b34ef
Michal Benatsky <mbenatsky@roblox.com>
2020-03-28 10:25:31 +0100

    some visual changes. Counter works

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ var app = new Vue({
       minusValue: 1
     },
     methods: {
-        addWorkout: function () {
-          this.amount = this.amount + this.plusValue;
-        },
-        subWorkout: function () {
+        click: function (e) {
+          let rect = e.target.getBoundingClientRect();
+          let sizeX = rect.right - rect.left;
+          let x = e.clientX - rect.left; 
+
+          if (x >= sizeX / 2)
+            this.amount = this.amount + this.plusValue;
+          else
             this.amount = this.amount - this.minusValue;
-          },
+        },
       }
   })
