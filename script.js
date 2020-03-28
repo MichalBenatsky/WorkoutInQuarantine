825fd585677cdaf0d1d6b4af39fb40d79cc13d98
Michal Benatsky <mbenatsky@roblox.com>
2020-03-28 11:09:43 +0100

    working component system

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+
+Vue.component('workoutcounter', {
+  props: ['type', 'imgsrc'],
+  data: function () {
+    return {
       amount: 0,
-      message: 'Hello Vue!',
-      type: 'PushUp',
       plusValue: 5,
       minusValue: 1
-    },
-    methods: {
-        click: function (e) {
-          let rect = e.target.getBoundingClientRect();
-          let sizeX = rect.right - rect.left;
-          let x = e.clientX - rect.left; 
+    }
+  },
+  template: ` <div class="counter">
+                <h2>{{ type }}</h2>
+                <h3>{{ amount }}</h3>
+                <div class="stack">
+                  <img class="stackUp" v-bind:src="imgsrc" v-on:click="click" >
+                </div>
+              </div>`,
+  methods: {
+    click: function (e) {
+      let rect = e.target.getBoundingClientRect();
+      let sizeX = rect.right - rect.left;
+      let x = e.clientX - rect.left; 
 
-          if (x >= sizeX / 2)
-            this.amount = this.amount + this.plusValue;
-          else
-            this.amount = this.amount - this.minusValue;
-        },
-      }
-  })
+      if (x >= sizeX / 2)
+        this.amount = this.amount + this.plusValue;
+      else
+        this.amount = this.amount - this.minusValue;
+
+      this.amount = Math.max(0, this.amount);
+    },
+  }
+})
+
+let app = new Vue({
+  el: '#app',
+
+})
