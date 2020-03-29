1290ec8647ab2fea24c5e1e093aa4d7dea3820b3
Michal Benatsky <mbenatsky@roblox.com>
2020-03-29 12:07:20 +0200

    Ok it does all I want but it doesn't look that great :(

--- script.js
+++ script.js
@@ -33,9 +33,19 @@ let app = new Vue({
       { id:2, type: 'squat', imgsrc:'img/squat.svg', amount: 0}
     ],
     plusValue: 5,
-    minusValue: 1
+    minusValue: 1,
+    uploadDataNeeded: false
   },
   el: '#app',
+  created: function () {
+    setInterval(function () {
+      if (this.uploadDataNeeded == true)
+      {
+        this.uploadData();
+        this.uploadDataNeeded = false;
+      }
+    }.bind(this), 5000); 
+  },
   methods: {
     eventChild: function(id, change) 
     {
@@ -43,6 +53,8 @@ let app = new Vue({
         this.workouts[id].amount = this.workouts[id].amount + this.plusValue;
       else
         this.workouts[id].amount = this.workouts[id].amount - this.minusValue;
+
+      this.uploadDataNeeded = true;
     },
     setData: function(jsonData)
     {
@@ -54,6 +66,22 @@ let app = new Vue({
       {
         this.workouts[i].amount = jsonData[this.workouts[i].type];
       }
+    },
+    uploadData: function()
+    {
+      let formData = { "person": $("#sportsmen").val(), "command" : "WRITE"};
+      for (let i = 0; i < this.workouts.length; ++i)
+        formData[this.workouts[i].type] = this.workouts[i].amount;
+
+      console.log(formData);
+
+      $.ajax({
+        type : 'POST',
+        url  : 'https://script.google.com/macros/s/AKfycbzdld1IiV4CPKCTHR3YqT8U-H7Kc_hpjdFZRWeJp8B5Zea24yQ/exec',
+        data : formData,
+        dataType : 'json',
+        encode : true
+      });
     }
   }
 })
@@ -83,9 +111,15 @@ function updateData()
 
 $("#sportsmen").change(function()
 {
+  window.localStorage.setItem("person", $("#sportsmen").val());
   updateData();
 });
 
 $( document ).ready(function() {
+  if (window.localStorage.getItem("person") != null)
+  {
+    $("#sportsmen").val(window.localStorage.getItem("person"));
+  }
+
   updateData();
 });
